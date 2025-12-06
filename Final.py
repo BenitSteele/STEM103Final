c4ea083ff3dc3ecd8f52090eedce860530561df5
BenitSteele <basteele2@students.everettcc.edu>
2025-12-06 05:50:05 +0000

    Updated final

--- Final.py
+++ Final.py
@@ -1,8 +1,3 @@
-
-import pandas as pd
-import csv
-from io import StringIO
-input_file = "Cities.csv"
-data = "NAME,DATE,TAVG\na,b,1\na,b,2\nc,d,3"
-pd.read_csv(StringIO(data))
+from Price_list import*
+price_list()
 
---
+++ Price_list.py
@@ -0,0 +1,17 @@
+import pandas as pd
+def price_list():
+    data = []
+    num_items_bought = int(input("Enter number of item(s) purchased: "))
+    repeat_program = True
+    while repeat_program:
+        for _ in range(num_items_bought):
+            bought_item = input("Enter item purchased: ")
+            cost = float(input("Enter cost of item: "))
+            data.append([bought_item, cost])
+            df_bought = pd.DataFrame(data, columns=['bought_item', 'cost'])
+            print("\nFinal DataFrame:")
+            print(df_bought)
+        responce = input("\nWould you like to continue? (Y/N): ")
+        if responce != "Y":
+            print("\nBye!")
+            break
