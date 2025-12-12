53180dc24eff7c89f9c96398c3c6728649169dd2
BenitSteele <basteele2@students.everettcc.edu>
2025-12-12 17:44:19 +0000

    Finished updated Final

--- Final.py
+++ Final.py
@@ -1,3 +1,3 @@
-from Price_list import*
-price_list()
+from Price_list import* # Importing the price_list function from Price_list.py
+price_list() # Calling the price_list function to execute the program
 
--- Price_list.py
+++ Price_list.py
@@ -1,20 +1,20 @@
-import pandas as pd
-def price_list():
-    data = []
+import pandas as pd # Grabbing the PANDAS library for DataFrame creation
+def price_list(): # Function to create a DataFrame of purchased items and their costs
+    data = [] # Empty list to store item data
     num_items_bought = int(input("Enter number of item(s) purchased: "))
     repeat_program = True
-    while repeat_program:
-        for _ in range(num_items_bought):
-            bought_item = input("Enter item purchased: ")
-            cost = float(input("Enter cost of item: "))
-            data.append([bought_item, cost])
+    while repeat_program: # Loop to allow multiple entries
+        for _ in range(num_items_bought): # Loop for the number of items purchased
+            bought_item = input("Enter item purchased: ") # User input for item name
+            cost = float(input("Enter cost of item: ")) # User input for item cost
+            data.append([bought_item, cost]) # Append item and cost to data list
             df_bought = pd.DataFrame(data, columns=['bought_item', 'cost'])
             print("\nFinal DataFrame:")
-            print(df_bought)
+            print(df_bought) # Display the DataFrame
         responce = input("\nWould you like to continue? (Y/N): ")
-        if responce == "Y":
+        if responce == "Y": # If user wants to continue
             num_items_bought = int(input("Enter number of item(s) purchased: "))
-        else:
+        else: # If user wants to stop
             repeat_program = False
             print("Bye!")
         
